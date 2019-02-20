360efa671dcc3214c6bb5b71a09a87ab98db3e5b
Gerardo Manzo <programmingmanzo@gmail.com>
2019-02-20 08:19:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,13 @@ end
 end
 
 def countdown_with_sleep(number)
-  while number > 0 
-  puts 
+  sleep 5
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+  
   
